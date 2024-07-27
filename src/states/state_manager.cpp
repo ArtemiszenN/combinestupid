f49a46911273156f32f96a0bd09b66b7d6500bed
ArtemiszenN <artemiszenn@gmail.com>
2024-07-28 04:33:44 +0800

    fixed bug with saving state by tracking usernames instead of all user data

--- src/states/state_manager.cpp
+++ src/states/state_manager.cpp
@@ -4,6 +4,7 @@ extern std::shared_ptr<User_state> user_state;
 State_manager::State_manager(int _save_interval_seconds, std::shared_ptr<User_state> _user_state) : save_interval_seconds(_save_interval_seconds), user_state(_user_state) {}
 void State_manager::start()
 {
+    std::this_thread::sleep_for(std::chrono::seconds(save_interval_seconds));
     while (true)
     {
         user_state->save();
